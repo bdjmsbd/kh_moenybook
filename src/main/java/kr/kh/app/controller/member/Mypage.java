8374286e14e06f4f8c6bb56588c23aeee1ea7612
kanggun99 <kanggun99@gmail.com>
2024-09-11 16:03:32 +0900

    수정

--- src/main/java/kr/kh/app/controller/member/Mypage.java
+++ src/main/java/kr/kh/app/controller/member/Mypage.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kr.kh.app.model.vo.MemberVO;
 import kr.kh.app.service.MemberService;
 //미완
 @WebServlet("/mypage")
